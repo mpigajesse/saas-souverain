301be47091439b4e8a0c519a44e9b508c394247d
Pmabouba27 <maboubapriscille@gmail.com>
2026-08-26 15:55:11 +0100

    feat(ss-client): raccordement crypto Mission A et vrai scellement Sealed Box X25519

--- core-rust/ss-crypto/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod error;
-mod dek;
-mod device_key;
-mod recovery;
-
-pub use error::CryptoError;
-pub use dek::Dek;
-pub use device_key::{DeviceKeyPair, DevicePublicKey};
-pub use recovery::{derive_recovery_key, generate_salt};
--- core-rust/ss-journal/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod entry;
-mod error;
-mod journal;
-
-pub use entry::JournalEntry;
-pub use error::JournalError;
-pub use journal::Journal;
